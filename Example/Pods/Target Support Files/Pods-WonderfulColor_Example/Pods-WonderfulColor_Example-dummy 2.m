581d653af4e4153f39249afc25ea92c98822b495
Rubin.Jo <dbqlsdl1234@naver.com>
2021-10-23 21:25:33 +0900

    [Begin] begin WonderfulColor with making WonderfulColor.swift. And delete test pods because for other test rule

--- Example/Pods/Target Support Files/Pods-WonderfulColor_Example/Pods-WonderfulColor_Example-dummy 2.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_WonderfulColor_Example : NSObject
-@end
-@implementation PodsDummy_Pods_WonderfulColor_Example
-@end
--- Example/WonderfulColor/ViewController.swift
+++ Example/WonderfulColor/ViewController.swift
@@ -7,18 +7,14 @@
 //
 
 import UIKit
+import WonderfulColor
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = WonderfulColor.white
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 }
 
